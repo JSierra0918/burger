43f70e1069a228bdf6e7232eb88ecc06d957bb60
Jorge Sierra <3d.jorgesierra@gmail.com>
2019-05-07 12:47:21 -0400

    init

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("California Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Dolphin Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Elephant Sliders", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Just Avocado burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Philly Cheesteak Burger", false);
