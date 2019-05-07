c22942de10538c3f8a2b9745ee97c5a81609be62
Jorge Sierra <3d.jorgesierra@gmail.com>
2019-05-07 14:09:31 -0400

    done with orm

---
+++ config/orm.js
@@ -0,0 +1,38 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+//object-relational mapping ================================
+
+var orm = {
+    selectAll: function (table, cb) {
+        var queryString ="SELECT * FROM ??";
+        connection.query( queryString, [table], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            console.log(result);
+        });
+    },
+
+    insertOne: function (table,burgerName, cb) {
+        var queryString = "INSERT INTO ?? (burger_name, devoured) VALUES ?";
+        connection.query(queryString, [table, burgerName], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            console.log(result);
+        });
+    },
+
+    updateOne : function (table, colName,value){
+        var queryString = "UPDATE ?? SET ?? = ? ";
+        connection.query(queryString,[table, colName,value],function(err, result){
+            if (err) {
+                throw err;
+            }
+            console.log(result);
+        });
+    }
+}
+
+module.exports = orm ;
